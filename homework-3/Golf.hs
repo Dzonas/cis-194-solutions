006d053556f53465e747846a53f73894b245b409
Jan Cybulski <13089097+Dzonas@users.noreply.github.com>
2022-03-08 21:29:32 +0100

    Add solution to ex.3 homework 3

--- homework-3/Golf.hs
+++ homework-3/Golf.hs
@@ -1,5 +1,8 @@
 module Golf where
 
+import           Data.Char
+import           Data.List
+
 skips :: [a] -> [[a]]
 skips xs = map takeModN [1 .. (length xs)]
  where
@@ -11,3 +14,15 @@ localMaxima xs =
     xs
     (drop 1 xs)
     (drop 2 xs)
+
+stars :: [Int] -> [String]
+stars xs
+  | any (> 0) xs = map (\x -> if x > 0 then '*' else ' ') xs
+  : stars (map (\x -> max (x - 1) 0) xs)
+  | otherwise = []
+
+histogram :: [Integer] -> String
+histogram xs =
+  unlines
+    $  (reverse . stars . map (length . (\x -> filter (== x) xs)) $ [0 .. 9])
+    ++ [replicate 10 '=', map intToDigit [0 .. 9]]
