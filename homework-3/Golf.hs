86c6c898787ce8b685be7383183633c647e1478c
Jan Cybulski <13089097+Dzonas@users.noreply.github.com>
2022-03-08 18:31:22 +0100

    Add solution to ex.2 homework 3

--- homework-3/Golf.hs
+++ homework-3/Golf.hs
@@ -4,3 +4,10 @@ skips :: [a] -> [[a]]
 skips xs = map takeModN [1 .. (length xs)]
  where
   takeModN n = map snd . filter ((== 0) . (`mod` n) . fst) $ zip [1 ..] xs
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima xs =
+  map (\(_, b, _) -> b) . filter (\(a, b, c) -> b > a && b > c) $ zip3
+    xs
+    (drop 1 xs)
+    (drop 2 xs)
