654cdfa27188062c5ecb072d41081a392cfa6ead
Jan Cybulski <13089097+Dzonas@users.noreply.github.com>
2022-03-09 19:53:21 +0100

    Add solution to ex.2 homework 4

--- homework-4/Homework.hs
+++ homework-4/Homework.hs
@@ -7,3 +7,19 @@ fun2' :: Integer -> Integer
 fun2' n = sum . filter even . takeWhile (/= 1) $ iterate
   (\x -> if even x then x `div` 2 else 3 * x + 1)
   n
+
+data Tree a = Leaf | Node Integer (Tree a) a (Tree a) deriving (Show, Eq)
+
+treeHeight :: Tree a -> Integer
+treeHeight Leaf           = 0
+treeHeight (Node n _ _ _) = n
+
+foldTree :: [a] -> Tree a
+foldTree []       = Leaf
+foldTree [x     ] = Node 0 Leaf x Leaf
+foldTree (x : xs) = Node height left x right
+ where
+  height = max (treeHeight left) (treeHeight right) + 1
+  left   = foldTree $ take n xs
+  right  = foldTree $ drop n xs
+  n      = length xs `div` 2
