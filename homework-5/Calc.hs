c7aacdcfb6b997ac8c4251afc660e8c955a94e1f
Jan Cybulski <13089097+Dzonas@users.noreply.github.com>
2022-03-17 21:36:46 +0100

    Add solution to ex.2 homework 5

--- homework-5/Calc.hs
+++ homework-5/Calc.hs
@@ -1,8 +1,14 @@
 module Calc where
 
 import           ExprT
+import           Parser
 
 eval :: ExprT -> Integer
 eval (Lit n         ) = n
 eval (Add left right) = eval left + eval right
 eval (Mul left right) = eval left * eval right
+
+evalStr :: String -> Maybe Integer
+evalStr s = do
+  x <- parseExp Lit Add Mul s
+  return (eval x)
