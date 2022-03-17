8a657e3eaa1151be32c6762e3059ad52318503f2
Jan Cybulski <13089097+Dzonas@users.noreply.github.com>
2022-03-17 21:52:58 +0100

    Add solution to ex.4 homework 5

--- homework-5/Calc.hs
+++ homework-5/Calc.hs
@@ -22,3 +22,27 @@ instance Expr ExprT where
   lit = Lit
   add = Add
   mul = Mul
+
+instance Expr Integer where
+  lit = id
+  add = (+)
+  mul = (*)
+
+instance Expr Bool where
+  lit n = n > 0
+  add = (||)
+  mul = (&&)
+
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+
+instance Expr MinMax where
+  lit = MinMax
+  add (MinMax left) (MinMax right) = MinMax (max left right)
+  mul (MinMax left) (MinMax right) = MinMax (min left right)
+
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+
+instance Expr Mod7 where
+  lit n = Mod7 (n `mod` 7)
+  add (Mod7 left) (Mod7 right) = Mod7 ((left + right) `mod` 7)
+  mul (Mod7 left) (Mod7 right) = Mod7 ((left * right) `mod` 7)
