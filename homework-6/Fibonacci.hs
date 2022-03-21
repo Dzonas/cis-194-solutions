bcf941fe956ea8563d94301ca4d2dffd454e9639
Jan Cybulski <13089097+Dzonas@users.noreply.github.com>
2022-03-21 20:14:38 +0100

    Add solution to ex.2 homework 6

--- homework-6/Fibonacci.hs
+++ homework-6/Fibonacci.hs
@@ -7,3 +7,10 @@ fib n = fib (n - 1) + fib (n - 2)
 
 fibs1 :: [Integer]
 fibs1 = map fib [0 ..]
+
+-- https://stackoverflow.com/a/21605696
+fibo :: Integer -> Integer -> [Integer]
+fibo a b = a : fibo b (a + b)
+
+fibs2 :: [Integer]
+fibs2 = fibo 0 1
