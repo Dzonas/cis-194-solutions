2f8fb4a357749f2fd352d75d7a964b07afbc9f09
Jan Cybulski <13089097+Dzonas@users.noreply.github.com>
2022-03-21 20:45:28 +0100

    Add solution to ex.3-5 homework 6

--- homework-6/Fibonacci.hs
+++ homework-6/Fibonacci.hs
@@ -14,3 +14,30 @@ fibo a b = a : fibo b (a + b)
 
 fibs2 :: [Integer]
 fibs2 = fibo 0 1
+
+data Stream a = Stream a (Stream a)
+
+streamToList :: Stream a -> [a]
+streamToList (Stream x xs) = x : streamToList xs
+
+instance Show a => Show (Stream a) where
+  show = show . take 20 . streamToList
+
+streamRepeat :: a -> Stream a
+streamRepeat n = Stream n (streamRepeat n)
+
+streamMap :: (a -> b) -> Stream a -> Stream b
+streamMap f (Stream x xs) = Stream (f x) (streamMap f xs)
+
+streamFromSeed :: (a -> a) -> a -> Stream a
+streamFromSeed f z = Stream z (streamFromSeed f (f z))
+
+nats :: Stream Integer
+nats = streamFromSeed succ 0
+
+divTest :: Integer -> Integer -> Integer
+divTest k n | n `mod` (2 ^ k) == 0 = divTest (k + 1) n
+            | otherwise            = k - 1
+
+ruler :: Stream Integer
+ruler = streamMap (divTest 0) $ streamFromSeed succ 1
